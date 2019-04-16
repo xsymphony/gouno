d5577096964c465cae16c6de833844338ed25961
xiongsyao <xiongshengyao@hotmail.com>
2019-04-16 09:45:34 +0800

    fix: use urlencoding to avoid '/'

--- utils/filehash.go
+++ utils/filehash.go
@@ -1,13 +1,13 @@
 package utils
 
 import (
-	"encoding/base64"
 	"crypto/sha256"
+	"encoding/base64"
 	"io"
 )
 
 func CalculateHash(r io.Reader) string {
 	h := sha256.New()
 	io.Copy(h, r)
-	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+	return base64.URLEncoding.EncodeToString(h.Sum(nil))
 }
--- utils/filehash_test.go
+++ utils/filehash_test.go
@@ -1,18 +1,18 @@
 package utils
 
 import (
-	"testing"
 	"strings"
+	"testing"
 )
 
 func TestCalculateHash(t *testing.T) {
-	var tests = []struct{
-		input string
+	var tests = []struct {
+		input  string
 		output string
-	} {
-		{"123", "pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM="},
-		{"中文.docx", "Pl7sZt8A+li9d5he5IRJkq8rAMCdnX2g5v2B3/RdXgI="},
-		{"", "47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="},
+	}{
+		{"123", "pmWkWSBCL51Bfkhn79xPuKBKHz__H6B-mY6G9_eieuM="},
+		{"中文.docx", "Pl7sZt8A-li9d5he5IRJkq8rAMCdnX2g5v2B3_RdXgI="},
+		{"", "47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU="},
 	}
 	for _, test := range tests {
 		r := CalculateHash(strings.NewReader(test.input))
